3055dac2fe0211550357bcac00fb1a3b839f93d2
Panelo56 <metainf12@mail.ru>
2024-12-14 11:55:53 +0300

    Initial commit

---
+++ src/test/java/ru/netology/test/AppTest.java
@@ -0,0 +1,48 @@
+package ru.netology.test;
+
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.databaseClean;
+
+public class AppTest {
+
+    @AfterAll
+    static void tearDown() {
+        databaseClean();
+    }
+
+    @Test
+    void shouldLoginSuccessful() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getCodeVerification();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    void shouldErrorLoggingRandomUser() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotificationVisiblity();
+    }
+
+    @Test
+    void shouldErrorIncorrectVerificationCode() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotificationVisiblity();
+
+    }
+}
